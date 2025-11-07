4317e8cc5545133c6a961e4a6811a3673ddbf328
tsiory17 <tsiory.r17@gmail.com>
2025-11-07 00:34:07 -0500

    Add useEffect in App.jsx to scroll up when using the pagination and Add a video file

--- movie_app/src/App.jsx
+++ movie_app/src/App.jsx
@@ -1,131 +1,138 @@
-import React, {useState, useEffect} from 'react';
-import {useDebounce} from "react-use";
-import Search from './components/Search.jsx'
+import React, { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
+import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import Header from "./components/Header.jsx";
 import Pagination from "./components/Pagination.jsx";
-import {getMoviesTrending, updateSearch} from "./lib/appwrite.js";
+import { getMoviesTrending, updateSearch } from "./lib/appwrite.js";
 
 const App = () => {
-    const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
-    const API_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
-    const API_KEY = import.meta.env.VITE_TMDB_KEY;
+  const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+  const API_SEARCH_URL = "https://api.themoviedb.org/3/search/movie";
+  const API_KEY = import.meta.env.VITE_TMDB_KEY;
 
-    const API_OPTIONS = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            authorization: `Bearer ${API_KEY}`
-        }
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      authorization: `Bearer ${API_KEY}`,
+    },
+  };
+
+  const [search, setSearch] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [debounced, setDebounced] = useState("");
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  const [pagination, setPagination] = useState(1);
+
+  useDebounce(
+    () => {
+      setDebounced(search);
+    },
+    1700,
+    [search]
+  );
+
+  const fetchMovies = async (query = "") => {
+    setLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = query
+        ? `${API_SEARCH_URL}?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}?sort_by=popularity.desc&page=${pagination}`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Could not fetch movies");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      if (data.Response === "false") {
+        setErrorMessage(data.Error || "Could not fetch movies");
+        setMovies([]);
+        return;
+      }
+      setMovies(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearch(query, data.results[0]);
+      }
+    } catch (e) {
+      setErrorMessage("Error fetching movies");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const [search, setSearch] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [debounced,setDebounced] = useState('');
-    const [searchedMovies, setSearchedMovies] = useState([])
-
-    const [pagination, setPagination] = useState(1);
-
-    useDebounce(() => {setDebounced(search);}, 1700,[search]);
-
-    const fetchMovies = async (query= '') => {
-        setLoading(true);
-        setErrorMessage('');
-        try {
-            const endpoint = query ?
-                `${API_SEARCH_URL}?query=${encodeURIComponent(query)}` : `${API_BASE_URL}?sort_by=popularity.desc&page=${pagination}`;
-
-            const response = await fetch(endpoint, API_OPTIONS);
-
-            if (!response.ok) {
-                throw new Error('Could not fetch movies');
-            }
-
-            const data = await response.json();
-            console.log(data);
-            if (data.Response === 'false') {
-                setErrorMessage(data.Error || "Could not fetch movies");
-                setMovies([]);
-                return;
-            }
-            setMovies(data.results || []);
-
-
-            if (query && data.results.length > 0) {
-                await updateSearch(query, data.results[0])
-            }
-
-        } catch (e) {
-            setErrorMessage('Error fetching movies');
-        } finally {
-            setLoading(false);
-        }
+  const loadMostSearched = async () => {
+    try {
+      const mostSearchedList = await getMoviesTrending();
+      setSearchedMovies(mostSearchedList);
+    } catch (e) {
+      console.error(e);
     }
+  };
+  useEffect(() => {
+    fetchMovies(debounced);
+  }, [debounced, pagination]);
 
-    const loadMostSearched = async () => {
-        try{
-            const mostSearchedList = await getMoviesTrending();
-            setSearchedMovies(mostSearchedList);
-        }catch(e){
-            console.error(e);
-        }
+  useEffect(() => {
+    loadMostSearched();
+  }, []);
+  useEffect(() => {
+    // Scroll to the "All Movies" section smoothly
+    const allMoviesSection = document.getElementById("all-movies");
+    if (allMoviesSection) {
+      allMoviesSection.scrollIntoView({ behavior: "smooth" });
     }
-    useEffect(() => {
-       fetchMovies(debounced);
-    }, [debounced,pagination]);
+  }, [pagination]);
 
-    useEffect(() => {
-        loadMostSearched();
-    },[]);
+  return (
+    <main>
+      <div className="pattern " />
+      <div className="wrapper bg-dark-100">
+        <Header setSearch={setSearch} search={search} />
 
+        {searchedMovies.length > 0 && (
+          <section className="trending">
+            <h2>Most Searched Movies</h2>
+            <ul>
+              {searchedMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url}></img>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
 
+        <section id="all-movies" className="all-movies">
+          <h1>All Movies</h1>
 
-    return (
-        <main>
-            <div className="pattern"/>
-            <div className="wrapper">
-                <Header setSearch={setSearch} search={search} />
+          <Pagination pagination={pagination} setPagination={setPagination} />
 
-                {searchedMovies.length > 0 && (
-
-                    <section className="trending">
-                        <h2>Most Searched Movies</h2>
-                        <ul>
-                            {searchedMovies.map((movie,index) => (
-                                <li key={movie.$id}>
-                                    <p>{index+1}</p>
-                                    <img src={movie.poster_url}></img>
-
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-                )
-
-                }
-
-
-                <section className="all-movies">
-                    <h1>All Movies</h1>
-                    {loading ? (
-                        <Spinner/>
-                    ) : errorMessage ? (
-                        <p className="text-red-500">{errorMessage}</p>
-                    ) : (
-                        <ul className="mt-20">
-                            {movies.map(movie => (
-                                <MovieCard key={movie.id} movie={movie}/>
-                            ))}
-                        </ul>
-                    )
-                    }
-                    <Pagination pagination={pagination} setPagination={setPagination} />
-                </section>
-            </div>
-        </main>
-    )
-}
-export default App
+          {loading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul className="mt-20">
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+          <Pagination pagination={pagination} setPagination={setPagination} />
+        </section>
+      </div>
+    </main>
+  );
+};
+export default App;
--- movie_app/src/components/Header.jsx
+++ movie_app/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Search from "./Search.jsx"
 const Header = ({search,setSearch}) => {
     return (
         <header>
-            <img src={"hero.png"} alt={"hero"}></img>
+            {/* <img src={"hero.png"} alt={"hero"}></img> */}
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy</h1>
             <Search search={search} setSearch={setSearch}/>
         </header>
